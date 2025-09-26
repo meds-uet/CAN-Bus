c59b6e4163af5abef5c8e4afe84b60ef439baf9f
ayesha_aqj <ayeshaqadir2252005@gmail.com>
2025-09-26 12:48:52 +0500

    bit timing

--- rtl/can_timing.sv
+++ rtl/can_timing.sv
@@ -13,20 +13,39 @@
 
 module can_timing (
 
-    input logic                                     rst_n,                    // reset
-    input logic                                     clk,                      // clock
+    input logic                                   rst_n,                    // reset
+    input logic                                   clk,                      // clock
 
-    input  wire type_reg2tim_s                      reg2tim_i,
-   
+    input logic                                   go_error_frame,
+    input logic                                   go_overload_frame,
+    input logic                                   send_ack,
+    input logic                                   rx,
+    input logic                                   tx,
+    input logic                                   tx_next,
+    input logic                                   transmitting,
+    input logic                                   transmitter,
+    input logic                                   rx_idle,
+    input logic                                   rx_inter,
+    input logic                                   go_tx,
 
-    output logic                                    frame_err_o
+    input logic                                   node_error_passive,
+
+    input  wire type_reg2tim_s                    reg2tim_i,
+  
+    //output logic                                  frame_err_o
+    output logic                                  sample_point,
+    output logic                                  sampled_bit,
+    output logic                                  sampled_bit_q,
+    output logic                                  tx_point,
+    output logic                                  hard_sync
+
 );
 
 // Registers to bit timing module interface 
 type_reg2tim_s                          reg2tim;
 
 logic [7:0]                             baud_div;
-logic [3:0]                             resync_delay;
+logic [2:0]                             resync_delay;
 logic [7:0]                             baud_count_ff, baud_count_next;
 logic [4:0]                             tq_counter_ff, tq_counter_next;
 logic                                   tq_clk_ff, tq_clk_next;
@@ -54,19 +73,19 @@ logic                                   allow_hard_sync;
 logic                                   latched_resync;
 logic                                   do_hard_sync;
 logic                                   do_resync;
+logic                                   resync_window;
+logic                                   is_tseg1_tx;
 
 // Signals for bit timing state machine
 type_can_bit_phase_e                    bit_phase_ff, bit_phase_next;
 
 /////////////////////////////////
-logic                  rst_n;
 
-assign reg2tim.tseg1 = time_segment1;
-assign reg2tim.tseg2 = time_segment2;
-assign reg2tim.sjw   = sync_jump_width;
-assign reg2tim.baud_prescaler = baud_r_presc;
+assign reg2tim.tseg1 = reg2tim_i.tseg1;
+assign reg2tim.tseg2 = reg2tim_i.tseg2;
+assign reg2tim.sjw   = reg2tim_i.sjw;
+assign reg2tim.baud_prescaler = reg2tim_i.baud_prescaler;
 
-assign rst_n = ~rst;
 /////////////////////////////////
 
 // Baud divisor based on prescaler	
